f19bf500c4f248ec445f1b135f5ad14d462540ce
Ohad Farkash <ohad@farkash.email>
2024-05-09 13:19:21 -0400

    init

---
+++ plugin/src/main/java/org/godotengine/plugin/vibration/GodotAndroidPlugin.kt
@@ -0,0 +1,37 @@
+// TODO: Update to match your plugin's package name.
+package org.godotengine.plugin.vibration
+
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import org.godotengine.godot.Godot
+import org.godotengine.godot.plugin.GodotPlugin
+import org.godotengine.godot.plugin.UsedByGodot
+
+class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
+
+    override fun getPluginName() = BuildConfig.GODOT_PLUGIN_NAME
+
+    @UsedByGodot
+    private fun vibrate(duration: Long, amplitude: Int) {
+        run {
+            val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                val vibratorManager =
+                    activity?.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                vibratorManager.defaultVibrator
+            } else {
+                @Suppress("DEPRECATION")
+                activity?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(duration, amplitude))
+            } else {
+                @Suppress("DEPRECATION")
+                vibrator.vibrate(duration)
+            }
+        }
+    }
+}
